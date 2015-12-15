023caddae9e36a61a9b824d0fcc7aaa0ca4df8c4
Marcelo Carmona <marcelo.carmona@fluxit.com.ar>
2015-12-15 01:35:56 -0300

    probando

--- web.js
+++ web.js
@@ -3,5 +3,5 @@
   var app = express();
 
   // app.use(express.logger('dev'));
-  app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+  app.use(express.static(__dirname + "/dist"));
   app.listen(process.env.PORT || 5000);
